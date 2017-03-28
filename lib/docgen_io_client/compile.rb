0cb57946f08058aa5de345ffea9f2f970e01daf7
Nicholas Stock <nick@gembani.com>
2017-03-28 21:19:44 +0200

    add pdf tex and log resources

--- lib/docgen_io_client/compile.rb
+++ lib/docgen_io_client/compile.rb
@@ -1,6 +1,18 @@
+require 'open-uri'
 module DocgenIoClient
   class Compile < Resource
     has_one :render
     resource_type :compiles
+    def pdf
+      open("http://api.docgen.io/compiles/#{id}/view/pdf?token=#{ENV.fetch('DOCGEN_API_KEY').freeze}")
+    end
+    
+    def tex
+      open("http://api.docgen.io/compiles/#{id}/view/tex?token=#{ENV.fetch('DOCGEN_API_KEY').freeze}")
+    end
+
+    def log
+      open("http://api.docgen.io/compiles/#{id}/view/log?token=#{ENV.fetch('DOCGEN_API_KEY').freeze}")
+    end
   end
 end
