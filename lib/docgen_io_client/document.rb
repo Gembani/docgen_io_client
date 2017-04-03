6c70bc443f8eb39516b3b3f0d9d6cf0a90198170
Nicholas Stock <nick@gembani.com>
2017-04-03 15:00:47 +0200

    no message

--- lib/docgen_io_client/document.rb
+++ lib/docgen_io_client/document.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 module DocgenIoClient
   class Document < Resource
-    has_one :render
+    has_one :template_render
     resource_type :documents
     def pdf
       open("#{Client.base_url}/compiles/#{id}/view/pdf?token=#{ENV.fetch('DOCGEN_API_KEY').freeze}")
