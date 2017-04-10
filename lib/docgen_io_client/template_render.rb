01999db1206bedee4d474f18d80469415e4a7a61
Nicholas Stock <nick@gembani.com>
2017-04-10 12:15:56 +0200

    adding attributes for docgen

--- lib/docgen_io_client/template_render.rb
+++ lib/docgen_io_client/template_render.rb
@@ -1,7 +1,19 @@
+require 'open-uri'
+
 module DocgenIoClient
   class TemplateRender < Resource
-    attributes :name, :params
+    attributes :name, :params, :pdf, :tex, :log
     has_one :template
     has_one :document
+    def pdf_object
+      open("#{pdf}/?token=#{ENV.fetch('DOCGEN_API_KEY').freeze}")
+    end
+    def tex_object
+      open("#{tex}/?token=#{ENV.fetch('DOCGEN_API_KEY').freeze}")
+    end
+    def log_object
+      open("#{log}/?token=#{ENV.fetch('DOCGEN_API_KEY').freeze}")
+    end
+
   end
 end
